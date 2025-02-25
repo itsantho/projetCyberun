ffa44184a2b811a5eb928b2c2d0bb88c6b14c672
itsantho <iem.anthony01@gmail.com>
2025-02-25 20:35:05 +0100

    1

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import './App.css';
 import { Header } from './components/Header';
 import { JsonFormsDemo } from './components/JsonFormsDemo';
 
+
 const App = () => {
   return (
     <>
       <Header />
-      <JsonFormsDemo />
+
+
+
     </>
   );
 };
